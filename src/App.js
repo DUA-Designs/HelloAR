296a005a3be93e776d9e7cbbf91556aae34bc52f
DUA_Designs <dua99mech.engr@gmail.com>
2024-02-23 15:34:13 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const myMedia=[v1,v2,v3,v4,v5,v6,v7,v8];
 function App() {
   const [page1,setPage1]=useState(true);
   const [likes,setLikes]=useState([0,0,0,0,0,0,0,0]);
+  const [clicked,setClicked]=useState([false,false,false,false,false,false,false,false]);
    
    
 
@@ -29,7 +30,16 @@ async function  hanleMainContent(){
   }
   function handleLikes(ind){
     let curr=likes;
+    let currClicked=clicked;
+    if(!clicked[ind]){
     curr[ind]+=1;
+  
+    }
+    else{
+      curr[ind]-=1;
+    }
+    currClicked[ind]=!currClicked[ind];
+    setClicked([...currClicked]);
     setLikes([...curr]);
 
   }
@@ -44,10 +54,10 @@ async function  hanleMainContent(){
 
   return (
     <div className=" container-fluid" id="rootChild">
-      {page1?<div className="  text-center text-white d-grid align-items-center" style={{height:"100vh"}}>
+      {page1?<div className="  text-center   d-grid align-items-center" style={{height:"100vh"}}>
          <h1>Hello AR. Welcome to my shorts App.</h1>
         <div className="  my-3 mx-auto " id="puppyContainer"><img src={puppy}  className="img-fluid" alt="puppy_Image"  id="puppy"></img></div>
-         <button id="start" className="btn btn-light col-10 col-lg-3 col-md-3 col-sm-10  mx-auto  my-3" onClick={hanleMainContent}>Start</button>  
+         <button id="start" className="btn btn-dark col-10 col-lg-3 col-md-3 col-sm-10  mx-auto  my-3" onClick={hanleMainContent}>Start</button>  
         </div>: 
      <div   id="mainContent" className="">
       
@@ -56,7 +66,7 @@ async function  hanleMainContent(){
                     {myMedia.map((item,index)=><div className="videoContainer   ">
                       <div className="position-relative  mx-auto  "><video className="  myVideos mx-auto rounded d-flex" controls autoPlay   key={index} loop > <source src={item}  type="video/mp4"></source>
                       Your browser does not support the video tag.</video>
-                      <div className="feedback "><button className="btn btn-light rounded-circle my-2" onClick={()=>handleLikes(index)}><i class="fa-solid fa-thumbs-up"></i></button>
+                      <div className="feedback "><button className="btn btn-dark rounded-circle my-2  " onClick={()=>handleLikes(index)}>{clicked[index]? <i className="fi fi-ss-social-network"></i>:<i className="fi fi-rs-social-network"></i>}</button>
                           <p>{likes[index]}</p>
                       
                        </div>
